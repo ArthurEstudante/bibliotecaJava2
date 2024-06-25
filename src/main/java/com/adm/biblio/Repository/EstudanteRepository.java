7bc1acbfff847d3ec579cb0d9d2ea46885aa6c8f
ArthurEstudante <160870282+ArthurEstudante@users.noreply.github.com>
2024-06-25 10:41:46 -0300

    parte 1 concluida

---
+++ src/main/java/com/adm/biblio/Repository/EstudanteRepository.java
@@ -0,0 +1,15 @@
+package com.adm.biblio.Repository;
+
+import com.adm.biblio.Entity.Estudante;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EstudanteRepository extends JpaRepository<Estudante,Long>{
+    
+    Estudante findByMatricula(Long matricula);
+    Estudante findByEmail(String email);
+    List<Estudante> findByNome(Long matricula);
+}
